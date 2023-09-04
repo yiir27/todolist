38063ce5e7c48497297169f21d324777df7c0175
yiir27 <camp.yui.c@gmail.com>
2023-09-04 14:13:42 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import React from 'react-dom';
+import style from './index.module.scss';
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <input placeholder='TODOを入力'/>
+        <button>追加</button>
+      </div>
+      <div></div>
+      <div></div>
+    </>
+  )
+}
+
+export default App;
